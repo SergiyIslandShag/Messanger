84cd2ed4be44fbd14ecc3864486bb1fab4fb10b9
mka <mka@A36PC13>
2024-11-06 20:26:21 +0200

    Create listbox

--- data_access/Entities/User.cs
+++ data_access/Entities/User.cs
@@ -14,8 +14,15 @@ namespace data_access.NewFolder
         public string PhoneNumber { get; set; }
         public string ServerAddress { get; set; }
         public int Port { get; set; }
-        public ICollection<MessageInfo> Messages { get; set; }  
-            
+        public ICollection<MessageInfo> Messages { get; set; }
+        public User(int id,string name, string num, string address, int port)
+        {
+            Id = id;
+            Name = name;
+            PhoneNumber = num;
+            ServerAddress = address;
+            Port = port;
+        }    
             
             
     }
