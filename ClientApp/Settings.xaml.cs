ad439e7df9afc7351b0102110751c8f17fc0980e
Shele_jc66@student.itstep.org <Shele_jc66@student.itstep.org>
2024-11-15 19:50:39 +0200

    Finaly create settings

--- ClientApp/Settings.xaml.cs
+++ ClientApp/Settings.xaml.cs
@@ -1,54 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Microsoft.EntityFrameworkCore;
 using data_access;
 using data_access.NewFolder;
 
 namespace ClientApp
 {
-	public partial class Settings : Window
-	{
+    public partial class Settings : Window
+    {
         private readonly MessangerDBContext dbContext;
-		private const string PremiumCode = "timlidtop";
-		private bool isPremiumActivated = false;
-		public Settings()
-		{
-			InitializeComponent();
+        private const string PremiumCode = "timlidtop";
+        private bool isPremiumActivated = false;
+        User userSetting;
+
+        public Settings()
+        {
+            InitializeComponent();
             dbContext = new MessangerDBContext();
-			userInfoListBox.ItemsSource= dbContext.Users.ToList();	
+            var users = dbContext.Users.ToList();
+            userInfoListBox.ItemsSource = users;
+        }
+        public Settings(User user)
+        {
+            InitializeComponent();
+            dbContext = new MessangerDBContext();
+            List<User> users = new List<User>();
+            users.Add(user);
+            userInfoListBox.ItemsSource = users;
         }
 
-
-		private void btnPremium_Click(object sender, RoutedEventArgs e)
-		{
-			if (txtPremiumCode.Text == PremiumCode)
-			{
-				if (!isPremiumActivated)
-				{
-
-					isPremiumActivated = true;
-					MessageBox.Show("Premium version activated!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-				}
-				else
-				{
-					MessageBox.Show("Premium version is already activated.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
-				}
-			}
-			else
-			{
-				MessageBox.Show("Invalid code. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-			}
-		}
+        private void btnPremium_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPremiumCode.Text == PremiumCode)
+            {
+                if (!isPremiumActivated)
+                {
+                    foreach (var user in dbContext.Users)
+                    {
+                        user.Name += " $"; 
+                    }
+                    userInfoListBox.ItemsSource = dbContext.Users.ToList();
+                    isPremiumActivated = true;
+                    MessageBox.Show("Premium version activated!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Premium version is already activated.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid code. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
